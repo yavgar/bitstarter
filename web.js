a6a1a6b21289f3eb8b1082235047ee46222ca1ac
Yair Avgar <yavgar@gmail.com>
2013-07-11 02:31:49 +0000

    Revert web.js back

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
-
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
-var buffer = new Buffer (fs.readFileSynch("index.html"), "utf-8");
+//var fs = require('fs');
+//var buffer = new Buffer (fs.readFileSynch("index.html"), "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send('sdf');//buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
