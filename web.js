5c5a1ef17a1ae0b4bc23c55fc199126c792868b5
Yair Avgar <yavgar@gmail.com>
2013-07-11 02:57:40 +0000

    Another try

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buffer = new Buffer (fs.readFileSynch("index.html"), "utf-8");
+  var buffer = new Buffer (fs.readFileSync("index.html"), "utf-8");
   response.send(buffer.toString());
 });
 
